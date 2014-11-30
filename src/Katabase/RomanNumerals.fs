5dac84f0670340663f30ae5680f28fdeb84acc5f
yelxe <trevor.exley@gmail.com>
2014-11-29 20:19:52 -0600

    Red-Green on toRoman
    Tests pass for Arabic to Roman conversion; further refactoring needed

--- src/Katabase/RomanNumerals.fs
+++ src/Katabase/RomanNumerals.fs
@@ -2,10 +2,40 @@
 
 module RomanNumerals =
     
-    let numerals = [(1, "I"); (5, "V"); (10, "X"); (50, "L"); (100, "C"); (500, "D"); (1000, "M")]
+    let I = "I"
+    let V = "V"
+    let X = "X"
+    let L = "L"
+    let C = "C"
+    let D = "D"
+    let M = "M"
+    let I' = "^I"
+    let V' = "^V"
+    let X' = "^X"
 
-    let toRoman an =
-        ""
+    let toRoman arabicNumeral =
+        
+        let sets = [(I,V,X); (X,L,C); (C,D,M); (M,V',X')]
 
-    let toArabic rn =
+        let tally x =
+            String.replicate x I
+
+        let replace (fnd) (rpl) (str:string) =
+            str.Replace((fnd |> String.concat ""), (rpl |> String.concat ""))
+        
+        let replaceFor (a, b, c) (str:string) =
+            str
+            |> replace [a;a;a;a;a]  [b]
+            |> replace [a;a;a;a]    [a;b]
+            |> replace [b;b]        [c]
+            |> replace [b;a;b]      [a;c]
+
+        arabicNumeral
+        |> tally
+        |> replaceFor sets.[0]
+        |> replaceFor sets.[1]
+        |> replaceFor sets.[2]
+        |> replaceFor sets.[3]
+
+    let toArabic romanNumeral =
         0
--- test/Katabase.Tests/RomanNumeralsTests.fs
+++ test/Katabase.Tests/RomanNumeralsTests.fs
@@ -8,6 +8,17 @@ module ``RomanNumerals Tests`` =
     
     module ``toRoman`` =
         
+//        [<TestCaseSource("convertDictionary")>]
+//        let ``Should convert correctly`` (a, r) =
+//            toRoman a |> should equal r
+//
+//        let convertDictionary () =
+//            [
+//                [|(1, "I")|];
+//                [|(2, "II")|]
+//            ]
+
+
         [<Test>]
         let ``1 to 10`` () =
             toRoman  1 |> should equal "I"
@@ -20,3 +31,26 @@ module ``RomanNumerals Tests`` =
             toRoman  8 |> should equal "VIII"
             toRoman  9 |> should equal "IX"
             toRoman 10 |> should equal "X"
+        
+        [<Test>]
+        let ``11 to 20`` () =
+            toRoman 11 |> should equal "XI"
+            toRoman 12 |> should equal "XII"
+            toRoman 13 |> should equal "XIII"
+            toRoman 14 |> should equal "XIV"
+            toRoman 15 |> should equal "XV"
+            toRoman 16 |> should equal "XVI"
+            toRoman 17 |> should equal "XVII"
+            toRoman 18 |> should equal "XVIII"
+            toRoman 19 |> should equal "XIX"
+            toRoman 20 |> should equal "XX"
+        
+        [<Test>]
+        let ``Other Numbers`` () =
+            toRoman 1999 |> should equal "MCMXCIX"
+            toRoman 99 |> should equal "XCIX"
+
+        [<Test>]
+        let ``High Numbers`` () =
+            toRoman 4000 |> should equal "M^V"
+            toRoman 6500 |> should equal "^VMD"
