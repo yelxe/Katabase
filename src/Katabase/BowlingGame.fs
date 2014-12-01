f2e5b44103d0ee2334ee9696bdafe5e285561e86
yelxe <trevor.exley@gmail.com>
2014-12-01 05:23:01 -0600

    Began BowlingGame
    Test for multiple strikes not passing, spares not handled correctly

---
+++ src/Katabase/BowlingGame.fs
@@ -0,0 +1,29 @@
+﻿namespace Katabase
+
+module BowlingGame =
+    
+    let frame (first, second) (score, spares) =
+        
+        let bonus =
+            match spares with
+            | 0  -> 0
+            | 1  -> first
+            | _  -> first + second
+
+        let earnedSpares =
+            match (first, second) with
+            | (10, 0)                 -> 2
+            | (f, s) when f + s = 10  -> 1
+            | _                       -> 0
+
+        let usedSpares =
+            match (first, second) with
+            | (10, 0)  -> 1
+            | _        -> 2
+            
+        let unusedSpares =
+            match spares - usedSpares with
+            | n when n < 0  -> 0
+            | a             -> a
+
+        (score + first + second + bonus, unusedSpares + earnedSpares)
---
+++ test/Katabase.Tests/BowlingGameTests.fs
@@ -0,0 +1,38 @@
+﻿namespace Katabase.Tests
+
+open NUnit.Framework
+open FsUnit
+open Katabase.BowlingGame
+
+module ``BowlingGame Tests`` =
+    
+    module ``Partial Games`` =
+ 
+        [<Test>]
+        let ``Two frames w/ no spares`` () =
+            (0, 0)
+            |> frame (3, 4)         // 7
+            |> frame (2, 6)         // 8
+            |> should equal (15, 0)
+
+        [<Test>]
+        let ``Spare followed by score`` () =
+            (0, 0)
+            |> frame (6, 4)         // 15
+            |> frame (5, 2)         // 7
+            |> should equal (22, 0)
+
+        [<Test>]
+        let ``Strike followed by score`` () =
+            (0, 0)
+            |> frame (10, 0)        // 17
+            |> frame (3, 4)         // 7
+            |> should equal (24, 0)
+
+        [<Test>]
+        let ``Two strikes followed by score`` () =
+            (0, 0)
+            |> frame (10, 0)        // 21
+            |> frame (10, 0)        // 13
+            |> frame (1, 2)         // 3
+            |> should equal (37, 0)
