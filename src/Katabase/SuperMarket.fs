89a57d9069032b7ed4f22e51c4cdc563add5c270
yelxe <trevor.exley@gmail.com>
2014-12-03 21:52:41 -0600

    Began supermarket

---
+++ src/Katabase/SuperMarket.fs
@@ -0,0 +1,29 @@
+﻿namespace Katabase
+
+module SuperMarket =
+    
+    [<Measure>] type Volume
+
+    [<Measure>] type mL
+    [<Measure>] type L
+    [<Measure>] type floz
+    [<Measure>] type gal
+
+    [<Measure>] type g
+    [<Measure>] type Kg
+    [<Measure>] type oz
+    [<Measure>] type lb
+
+    [<Measure>] type item
+
+    type Unit<[<Measure>] 'u> = float<'u>
+
+    type PricePerUnit<[<Measure>] 'u> = (float*float<'u>)
+
+    let mLperL:float<mL/L> = 1000.0<mL/L>
+    let gperKg:float<g/Kg> = 1000.0<g/Kg>
+
+    let convertPrice (f:PricePerUnit<'m>) (t:Unit) =
+        f
+
+    //setPriceFor (2.25, 1.0<L>) |> ignore
