e756cec6940feb439a9efc4a3150eeecabddcb26
Trevor Exley <texley@winnipeg.ca>
2014-12-09 15:54:24 -0600

    Done

--- src/Katabase/BowlingGame.fs
+++ src/Katabase/BowlingGame.fs
@@ -6,10 +6,10 @@ module BowlingGame =
     type Game = (Frame)list
     
     let (|Strike|Spare|Lame|) (a, b) = 
-           match a + b with
-           | 10 when a = 10 -> Strike
-           | 10 -> Spare
-           | _  -> Lame
+        match a + b with
+        | 10 when a = 10 -> Strike
+        | 10 -> Spare
+        | _  -> Lame
 
     let NewGame = []
 
@@ -17,22 +17,15 @@ module BowlingGame =
     
     let score (game:Game) =
                 
-        let scoreNextRoll = function
-        | (a,_)::_  -> a
-        | []        -> 0
-
-        let scoreNextTwoRolls = function
-        | Strike::(c,_)::_  -> 10 + c
-        | (a,b)::_          -> a + b
-        | []                -> 0
-
         let rec scoreFrames i frames  =
+            let next = scoreFrames (i + 1)
             match i, frames with
             | 10, _            -> 0
             | _, []            -> 0
-            | _, Strike::rest  -> 10 + scoreNextTwoRolls rest + scoreFrames (i + 1) rest
-            | _, Spare::rest   -> 10 + scoreNextRoll rest + scoreFrames (i + 1) rest
-            | _, (a,b)::rest   -> a + b + scoreFrames (i + 1) rest
+            | _, Strike::Strike::(a,b)::rest  -> 20 + a + next ((10,0)::(a,b)::rest)
+            | _, Strike::(a,b)::rest          -> 10 + a + b + next ((a,b)::rest)
+            | _, Spare::(a,b)::rest           -> 10 + a + next ((a,b)::rest)
+            | _, (a,b)::rest                  -> a + b + next rest
 
         game
         |> scoreFrames 0
