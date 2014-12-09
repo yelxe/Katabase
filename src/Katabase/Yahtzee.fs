bdefd2a0435ee436963f49d0851600c22ecb33f9
yelxe <trevor.exley@gmail.com>
2014-12-09 06:31:26 -0600

    Score for dice number tests pass

---
+++ src/Katabase/Yahtzee.fs
@@ -0,0 +1,29 @@
+﻿namespace Katabase
+
+module Yahtzee =
+    
+    type Roll = (int*int*int*int*int)
+
+    let toList (roll:Roll) =
+        let r1, r2, r3, r4, r5 = roll
+        [r1;r2;r3;r4;r5]
+
+    let toSeq (roll:Roll) =
+        let r1, r2, r3, r4, r5 = roll
+        seq [r1;r2;r3;r4;r5]
+
+    let scoreNumber n (roll:Roll) =
+        roll
+        |> toSeq
+        |> Seq.filter((=) n)
+        |> Seq.sum
+        
+    let countNumber n (roll:Roll) =
+        roll
+        |> toSeq
+        |> Seq.filter((=) n)
+        |> Seq.length
+
+//    let scoreThreeOfKind (roll:Roll) =
+//        roll
+//        |> toList
---
+++ test/Katabase.Tests/YahtzeeTests.fs
@@ -0,0 +1,28 @@
+﻿namespace Katabase.Tests
+
+open NUnit.Framework
+open FsUnit
+open Katabase.Yahtzee
+
+module ``Yahtzee Tests `` =
+    
+    module `` scoreNumber `` =
+        
+        [<Test>]
+        let ``Score for 1 thru 6`` () =
+            let cases =
+                [
+                    (1, (1,2,3,4,5),  1); (1, (2,1,4,6,1),  2); (1, (1,1,1,1,1),  5);
+                    (2, (1,2,3,4,5),  2); (2, (2,1,4,6,2),  4); (2, (2,2,2,2,2), 10)
+                ]
+            for (number, roll, expected) in cases do
+                scoreNumber number roll |> should equal expected
+
+    module `` hasEnough `` =
+        
+        [<Test>]
+        let ``Test`` () =
+            countNumber 6 (6,6,3,1,5) |> should equal 2
+            countNumber 6 (6,6,6,1,5) |> should equal 3
+            countNumber 6 (6,2,5,1,5) |> should equal 1
+        
