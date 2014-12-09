4fc5f68920ad0456cfa0697e66874fbfde6743e9
Trevor Exley <texley@winnipeg.ca>
2014-12-09 15:37:44 -0600

    Added active patterns for strike and spair

--- src/Katabase/BowlingGame.fs
+++ src/Katabase/BowlingGame.fs
@@ -5,6 +5,12 @@ module BowlingGame =
     type Frame = (int*int)
     type Game = (Frame)list
     
+    let (|Strike|Spare|Lame|) (a, b) = 
+           match a + b with
+           | 10 when a = 10 -> Strike
+           | 10 -> Spare
+           | _  -> Lame
+
     let NewGame = []
 
     let playFrame frame game = List.append game [frame]
@@ -16,17 +22,17 @@ module BowlingGame =
         | []        -> 0
 
         let scoreNextTwoRolls = function
-        | (10,0)::(c,_)::_  -> 10 + c
+        | Strike::(c,_)::_  -> 10 + c
         | (a,b)::_          -> a + b
         | []                -> 0
 
         let rec scoreFrames i frames  =
             match i, frames with
-            | 10, _                           -> 0
-            | _, []                           -> 0
-            | _, (10,0)::rest                 -> 10 + scoreNextTwoRolls rest + scoreFrames (i + 1) rest
-            | _, (a,b)::rest when a + b = 10  -> 10 + scoreNextRoll rest + scoreFrames (i + 1) rest
-            | _, (a,b)::rest                  -> a + b + scoreFrames (i + 1) rest
+            | 10, _            -> 0
+            | _, []            -> 0
+            | _, Strike::rest  -> 10 + scoreNextTwoRolls rest + scoreFrames (i + 1) rest
+            | _, Spare::rest   -> 10 + scoreNextRoll rest + scoreFrames (i + 1) rest
+            | _, (a,b)::rest   -> a + b + scoreFrames (i + 1) rest
 
         game
         |> scoreFrames 0
