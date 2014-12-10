e3bcb0861a4e677bd8e2e5262e7da95d1ecab92d
yelxe <trevor.exley@gmail.com>
2014-12-10 05:32:30 -0600

    Cleanup

--- src/Katabase/BowlingGame.fs
+++ src/Katabase/BowlingGame.fs
@@ -7,9 +7,9 @@ module BowlingGame =
     
     let (|Strike|Spare|Lame|) (a, b) = 
         match a + b with
-        | 10 when a = 10 -> Strike
-        | 10 -> Spare
-        | _  -> Lame
+        | 10 when a = 10  -> Strike
+        | 10              -> Spare
+        | _               -> Lame
 
     let NewGame = []
 
@@ -18,14 +18,16 @@ module BowlingGame =
     let score (game:Game) =
                 
         let rec scoreFrames i frames  =
+            
             let next = scoreFrames (i + 1)
+            
             match i, frames with
-            | 10, _            -> 0
-            | _, []            -> 0
-            | _, Strike::Strike::(a,b)::rest  -> 20 + a + next ((10,0)::(a,b)::rest)
-            | _, Strike::(a,b)::rest          -> 10 + a + b + next ((a,b)::rest)
-            | _, Spare::(a,b)::rest           -> 10 + a + next ((a,b)::rest)
-            | _, (a,b)::rest                  -> a + b + next rest
+            | 10, _                           ->  0
+            | _, []                           ->  0
+            | _, Strike::Strike::(a,b)::rest  -> 20  +  a        +  next ((10,0)::(a,b)::rest)
+            | _, Strike::(a,b)::rest          -> 10  +  a  +  b  +  next ((a,b)::rest)
+            | _, Spare::(a,b)::rest           -> 10  +  a        +  next ((a,b)::rest)
+            | _, (a,b)::rest                  ->  a  +  b        +  next rest
 
         game
         |> scoreFrames 0
